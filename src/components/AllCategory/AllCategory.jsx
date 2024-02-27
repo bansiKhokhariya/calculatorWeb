c86e14a8e246972b9e2c2eb401af0cf93f3948a9
bansi-hkapps <bansi.hkapps@gmail.com>
2024-02-27 12:52:44 +0530

    all category fiance focus change

--- src/components/AllCategory/AllCategory.jsx
+++ src/components/AllCategory/AllCategory.jsx
@@ -45,8 +45,8 @@ const AllCategory = () => {
                         </li>
                         <li className='category-inner-item'>Loan Calculator</li>
                         <li className='category-inner-item'>
-                            <input className="setting-menu-input" type="checkbox" name="setting-menu-item" id="setting-menu-item-7" />
-                            <label htmlFor="setting-menu-item-7">
+                            <input className="setting-menu-input" type="checkbox" name="setting-menu-item" id="setting-menu-item-3" />
+                            <label htmlFor="setting-menu-item-3">
                                 <svg className='down-arrow-svg' xmlns="http://www.w3.org/2000/svg" width="15" height="10" viewBox="0 0 15 10" fill="none">
                                     <path d="M7.49997 9.30044C7.23114 9.30044 6.96235 9.19779 6.75739 8.99294L0.307713 2.54319C-0.102571 2.13291 -0.102571 1.46771 0.307713 1.05759C0.71783 0.647471 1.3829 0.647471 1.79322 1.05759L7.49997 6.76468L13.2068 1.05779C13.617 0.64767 14.2821 0.64767 14.6921 1.05779C15.1026 1.46791 15.1026 2.13311 14.6921 2.54339L8.24256 8.99313C8.0375 9.19803 7.76871 9.30044 7.49997 9.30044Z" fill="#007B42" />
                                 </svg>

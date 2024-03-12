857f3fd7ec87f5fdbb359bee3082d890fe1e98b7
bansi-hkapps <bansi.hkapps@gmail.com>
2024-03-12 16:58:08 +0530

    button active

--- src/components/Calculator/BasicCalculator/BasicCalculator.jsx
+++ src/components/Calculator/BasicCalculator/BasicCalculator.jsx
@@ -109,7 +109,7 @@ const BasicCalculator = (props) => {
     };
 
     const handleDeleteHistory = () => {
-        localStorage.removeItem('savedResults');
+        localStorage.removeItem('basicCalculatorHistory');
         setSavedResults([]);
     };
 
